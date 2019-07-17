7c1d4032533f5e319bbbf14221bd742a3d7192a2
Matthew Eng <matthew.eng2@gmail.com>
2019-07-16 23:26:43 -0400

    Put all scraping and sql code into single function to loop through list of profile urls

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
+from random import randint
+from time import sleep
 
 import conf
 
@@ -103,10 +105,6 @@ urls = [
     'https://www.instagram.com/hiltoncarter/',
 ]
 
-instagram = InstagramScraper(urls[0])
-post_metrics = instagram.post_metrics()
-page_metrics = instagram.page_metrics()
-
 def connect_to_rds():
     ''' Connects to RDS and returns connection '''
     engine = create_engine(
@@ -164,45 +162,61 @@ def create_tables(drop_table=False):
     conn.close()
 
 create_tables(drop_table=True)
-conn = connect_to_rds()
 
-update_time = datetime.now().isoformat()
+def add_ig_data_from_urls(urls):
+    '''
+    Takes in a list of Instagram profile urls and parses post and profile
+    information and inserts into database
+    urls: list
+    '''
 
-# Add post metrics
-for metric in post_metrics[:1]:
-    i_id = str(metric['id'])
-    i_post_time = datetime.fromtimestamp(
-        metric['taken_at_timestamp']).isoformat()
-    i_likes = int(metric['edge_liked_by']['count'])
-    i_comments = int(metric['edge_media_to_comment']['count'])
-    i_media = metric['display_url']
-    i_video = bool(metric['is_video'])
+    conn = connect_to_rds()
 
-    insert_sql = """INSERT INTO post_metrics
-                    (post_id, post_time, update_time, post_likes, post_comments, post_media, post_is_video)
-                    VALUES ({}, '{}', '{}', {}, {}, '{}', {})
-                """.format(i_id, i_post_time, update_time, i_likes, i_comments, i_media, i_video)
-    conn.execute(insert_sql)
+    for url in urls:
+        print(url)
+        sleep(randint(10,100))
+        instagram = InstagramScraper(url)
+        post_metrics = instagram.post_metrics()
+        page_metrics = instagram.page_metrics()
 
-# Add page metrics
-user_id = str(page_metrics['id'])
-username = page_metrics['username']
-update_time = datetime.now().isoformat()
-bio = page_metrics['biography']#.replace('\n', '')
-video_timeline = page_metrics['edge_felix_video_timeline']
-follows = page_metrics['edge_follow']
-followers = page_metrics['edge_followed_by']
-media_collections = page_metrics['edge_media_collections']
-mutual_followed_by = page_metrics['edge_mutual_followed_by']
-saved_media = page_metrics['edge_saved_media']
+        # Add post metrics
+        update_time = datetime.now().isoformat()
+        for metric in post_metrics[:1]:
+            i_id = str(metric['id'])
+            i_post_time = datetime.fromtimestamp(
+                metric['taken_at_timestamp']).isoformat()
+            i_likes = int(metric['edge_liked_by']['count'])
+            i_comments = int(metric['edge_media_to_comment']['count'])
+            i_media = metric['display_url']
+            i_video = bool(metric['is_video'])
+
+            insert_sql = """INSERT INTO post_metrics
+                            (post_id, post_time, update_time, post_likes, post_comments, post_media, post_is_video)
+                            VALUES ({}, '{}', '{}', {}, {}, '{}', {})
+                        """.format(i_id, i_post_time, update_time, i_likes, i_comments, i_media, i_video)
+            conn.execute(insert_sql)
+
+        # Add page metrics
+        user_id = str(page_metrics['id'])
+        username = page_metrics['username']
+        update_time = datetime.now().isoformat()
+        bio = page_metrics['biography']#.replace('\n', '')
+        video_timeline = page_metrics['edge_felix_video_timeline']
+        follows = page_metrics['edge_follow']
+        followers = page_metrics['edge_followed_by']
+        media_collections = page_metrics['edge_media_collections']
+        mutual_followed_by = page_metrics['edge_mutual_followed_by']
+        saved_media = page_metrics['edge_saved_media']
 
 
-insert_sql = """INSERT INTO page_metrics
-                (user_id, username, update_time, biography , video_timeline, follows, followers,
-                media_collections, mutual_followed_by, saved_media)
-                VALUES ({}, '{}', '{}', '{}', {}, {}, {}, {}, {}, {})
-            """.format(user_id, username, update_time, bio, video_timeline, follows, followers,
-                       media_collections, mutual_followed_by, saved_media)
-conn.execute(insert_sql)
+        insert_sql = """INSERT INTO page_metrics
+                        (user_id, username, update_time, biography , video_timeline, follows, followers,
+                        media_collections, mutual_followed_by, saved_media)
+                        VALUES ({}, '{}', '{}', '{}', {}, {}, {}, {}, {}, {})
+                    """.format(user_id, username, update_time, bio, video_timeline, follows, followers,
+                               media_collections, mutual_followed_by, saved_media)
+        conn.execute(insert_sql)
 
-conn.close()
+    conn.close()
+
+add_ig_data_from_urls(urls)
