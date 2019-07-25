0e1ad81cd0938afea7ec674be58ab6d8cc37d46e
Matthew Eng <matthew.eng2@gmail.com>
2019-07-24 21:17:19 -0400

    Join page_metrics information and create new features for ML model

--- label.py
+++ label.py
@@ -31,14 +31,30 @@ def get_unlabeled_data_csv(filepath):
     engine = connect_to_rds()
 
     query = """
-    SELECT * from post_metrics;
+    SELECT * from post_metrics post
+        JOIN page_metrics page
+            ON page.user_id=post.user_id;
     """
 
     # execute the query and assign it to a pandas dataframe
     df = pd.read_sql_query(query, engine)
-    df['good_post'] = 1
+
+    # Feature engineering
+    def days_since_posted(row):
+        return (datetime.now() - datetime.strptime(str(row['post_time']), '%Y-%m-%d %H:%M:%S')).days
+
+    df['engagement_rate'] = df['post_likes'] / df['followers']* 100
+    df['num_#'] = df['post_caption'].str.count('#')
+    df['num_@'] = df['post_caption'].str.count('@')
+    df['caption_length'] = df['post_caption'].str.len()
+    df['days_since_post_date'] = df.apply(lambda row: days_since_posted(row), axis=1)
+    df['ratio_comments_days_posted'] = df['post_comments'] / df['days_since_post_date']
+    df['ratio_likes_days_posted'] = df['post_likes'] / df['days_since_post_date']
+
+    df['good_post'] = 0
     df['flag'] = flag_posts(df)
-    df.to_csv(filepath)
+    df.to_csv(filepath, index=False)
+
     return df
 
 def flag_posts(df):
