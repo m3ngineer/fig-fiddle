3cbd085063043c10d819e65172fdb8efb364ad62
Matthew Eng <matthew.eng2@gmail.com>
2019-08-03 19:20:52 -0400

    Fill in select_post function; fix several typos

--- post.py
+++ post.py
@@ -6,50 +6,66 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep, strftime
 from random import randint
 import pandas as pd
+import os
+from InstagramAPI import InstagramAPI
+import requests
 
 import conf
 
 def create_post():
     '''
-    Create a post and post to instagram
+    Create a post object and post to instagram
     '''
 
     # Select a post from database
-    engine = connect_to_rds(engine=True)
-    query = """
-        SELECT * from post_metrics post
-            JOIN page_metrics page
-                ON page.user_id=post.user_id
-            WHERE post.good_post=1
-                AND post.post_flag=0;
-        """
-    df = pd.read_sql_query(query, engine)
-    rand_post_i = random.choice(df.index)
-    post = df[rand_post_i]
+    post = select_post()
+    print(post)
+    # Save media from post
+    img_path = 'images/image.jpg'
+    img_url = post['post_media']
+    img_data = requests.get(img_url).content
+    with open(img_path, 'wb') as handler:
+        handler.write(img_data)
 
-    # generate a post_object
-    media = post['media']
-    caption = caption = generate_caption(post)
+    # Generate caption
+    caption = generate_caption(post)
 
-    # post to instagram
-        # connect to instagram
-        # post to account
+    # Post to instagram
+    instaAPI = InstagramAPI(conf.insta_handle, conf.insta_password)
+    print('logging into instagram...')
+    instaAPI.login()
+    sleep(5)
+    print('uploading photo...')
+    print(caption)
+    instaAPI.uploadPhoto(img_path, caption='testing')
 
     # Update post flag
-    update_post_flag(rand_post_i)
+    # update_post_flag(rand_post_i)
 
 def select_post():
     '''
     Select a post from database that hasn't been used yet
+    Return: a dataframe of information for selected post
     '''
 
-    # connect to database
-    # find post that hasn't been updated yet
-    # return post
+    engine = connect_to_rds(return_engine=True)
+    query = """
+        SELECT * from post_metrics post
+            JOIN page_metrics page
+                ON page.user_id=post.user_id
+            WHERE post.post_label_predict=1
+                AND post.post_unuseable_flag=0
+                AND post.posted_flag=0;
+        """
+    df = pd.read_sql_query(query, engine)
+    rand_post_i = random.choice(df.index)
+    post = df.iloc[rand_post_i]
+
+    return post
 
 def update_post_flag(post_index):
     '''
-    Update postpost_flag in database to 1
+    Update post.post_flag in database to 1
     '''
     conn = connect_to_rds()
     query = """
@@ -72,7 +88,7 @@ def generate_caption(profile_data):
             all_lines = fh.readlines()
             return random.choice(all_lines)
 
-    def generate_hastags():
+    def generate_hashtags():
         filename = 'hashes.txt'
         with open(filename, 'r') as fh:
             all_lines = fh.readlines()
@@ -93,9 +109,19 @@ def generate_caption(profile_data):
 
     return caption
 
-def login():
+def start_driver():
     chromedriver_path = '/Users/mattheweng/bin/chromedriver' # Change this to your own chromedriver path!
-    driver = webdriver.Chrome(executable_path=chromedriver_path)
+    # driver = webdriver.Chrome(executable_path=chromedriver_path)
+
+    mobile_emulation = { "deviceName": "Nexus 5" }
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options, desired_capabilities=chrome_options.to_capabilities())
+
+    return driver
+
+def login(driver):
+
     sleep(2)
     driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
     sleep(3)
@@ -105,11 +131,20 @@ def login():
     password = driver.find_element_by_name('password')
     password.send_keys(conf.insta_password)
 
-    button_login = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
+    button_login = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div/div/div/form/div[6]/button/div')
     button_login.click()
     sleep(3)
 
     notnow = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[3]/button[2]')
     notnow.click() #comment these last 2 lines out, if you don't get a pop up asking about notifications
 
-login()
+def make_post(image_path, caption):
+
+    instaAPI = InstagramAPI(conf.insta_handle, conf.insta_password)
+    instaAPI.login()
+    sleep(5)
+    photo_path = image_path
+    print('uploading photo...')
+    instaAPI.uploadPhoto(photo_path, caption=caption)
+
+create_post()
