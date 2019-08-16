b013195202e53c7a47ef74028287af3fc54639c1
Matthew Eng <matthew.eng2@gmail.com>
2019-08-15 21:35:43 -0400

    Begin to build out get_following

--- follow.py
+++ follow.py
@@ -10,16 +10,25 @@ class GetFollowers():
         self.instaAPI = InstagramAPI(conf.insta_handle, conf.insta_password)
         self.instaAPI.login()
 
-    def get_following(username_id):
+    def get_following(self, username_id, max_id=''):
         ''' gets the Followers for a given account '''
 
-        results = self.getUserFollowers(username_id, maxid='')
+        results = self.instaAPI.getUserFollowers(username_id, maxid='')
+#         while 1:
+#             self.getUserFollowers(usernameId, next_max_id)
+#             temp = self.LastJson
+
+#             for item in temp["users"]:
+#                 followers.append(item)
+
+#             if temp["big_list"] is False:
+#                 return followers
+#             next_max_id = temp["next_max_id"]
 
         return results
 
-    def follow_users(user_ids):
+    def follow_users(self, user_ids):
         ''' Follow random sample of users from list '''
 
         for user_id in user_ids:
             self.instaAPI.follow(user_id)
-            
