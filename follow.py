7f65c40e1379c3fdcb58cea8efe958b405c22d8b
Matthew Eng <matthew.eng2@gmail.com>
2019-08-16 20:22:02 -0400

    Add limit to get_following; create get_list_of_users to access user ids from db

--- follow.py
+++ follow.py
@@ -1,34 +1,59 @@
 from InstagramAPI import InstagramAPI
 import pandas as pd
 
+from db import connect_to_rds
 import conf
 
-
 class GetFollowers():
+    ''' Returns followers for a list of users '''
 
     def __init__(self):
         self.instaAPI = InstagramAPI(conf.insta_handle, conf.insta_password)
         self.instaAPI.login()
+        self.followers = []
 
-    def get_following(self, username_id, max_id=''):
+    def get_following(self, username_id, max_pages=10, max_id=''):
         ''' gets the Followers for a given account '''
 
-        results = self.instaAPI.getUserFollowers(username_id, maxid='')
-#         while 1:
-#             self.getUserFollowers(usernameId, next_max_id)
-#             temp = self.LastJson
+        next_max_id = ''
+        i = 0
 
-#             for item in temp["users"]:
-#                 followers.append(item)
+        while i <= max_pages:
+            i+= 1
+            self.instaAPI.getUserFollowers(username_id, maxid=next_max_id)
+            temp = self.instaAPI.LastJson
 
-#             if temp["big_list"] is False:
-#                 return followers
-#             next_max_id = temp["next_max_id"]
+            for item in temp['users']:
+                self.followers.append(item)
 
-        return results
+            if temp["big_list"] is False:
+                return self.followers
+
+            next_max_id = temp["next_max_id"]
+
+        return self.followers
 
     def follow_users(self, user_ids):
         ''' Follow random sample of users from list '''
 
         for user_id in user_ids:
             self.instaAPI.follow(user_id)
+
+def get_list_of_users():
+    ''' Returns list of user ids from database '''
+
+    conn = connect_to_rds()
+    sql = '''
+    SELECT DISTINCT user_id FROM page_metrics
+    '''
+
+    r = conn.execute(sql).fetchall()
+    r = [item[0] for item in r]
+
+    return r
+
+if "__name__" == "__main__":
+
+    gf = GetFollowers()
+    followers = gf.get_following()
+    gf.follow_user(followers)
