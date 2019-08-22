11534868e14ce4fba21f499f3e0893d9bcae4116
Matthew Eng <matthew.eng2@gmail.com>
2019-08-22 18:50:42 -0400

    Add self_following list to prevent following users already followed

--- follow.py
+++ follow.py
@@ -1,5 +1,6 @@
 from InstagramAPI import InstagramAPI
 import pandas as pd
+from time import sleep
 
 from db import connect_to_rds
 import conf
@@ -8,26 +9,33 @@ class GetFollowers():
     ''' Returns followers for a list of users '''
 
     def __init__(self):
+        self.user_id = '16389780544'
         self.instaAPI = InstagramAPI(conf.insta_handle, conf.insta_password)
         self.instaAPI.login()
         self.followers = []
+        # Add variable holding current followers
+        self.self_followers = self.get_following(self.user_id)
 
     def get_following(self, username_id, max_pages=1, max_id=''):
         ''' gets the Followers for a given account '''
 
         next_max_id = ''
         i = 0
+        results = []
 
-        print('test')
+        print('adding users...')
         while i <= max_pages:
             i+= 1
             self.instaAPI.getUserFollowers(username_id, maxid=next_max_id)
             temp = self.instaAPI.LastJson
 
             for item in temp['users']:
-                self.followers.append(item)
+                results.append(item)
 
             if temp["big_list"] is False:
+                followers = [str(user_info['pk']) for user_info in results]
+                self.followers = followers
+
                 return self.followers
 
             next_max_id = temp["next_max_id"]
@@ -37,8 +45,15 @@ class GetFollowers():
     def follow_users(self, user_ids):
         ''' Follow random sample of users from list '''
 
+        print('user_ids: {}'.format(user_ids))
+        i = 0
         for user_id in user_ids:
-            self.instaAPI.follow(user_id)
+            i += 1
+            sleep(2)
+            if user_id not in self.self_followers and i <= 5:
+                print('Following user {}'.format(user_id))
+                self.instaAPI.follow(user_id)
+                print('Followed user {}'.format(user_id))
 
     def unfollow_users(self, num_user_to_unfollow=10):
         ''' Unfollow random sample of users from following '''
@@ -66,7 +81,7 @@ if __name__ == "__main__":
     # print(get_list_of_users())
 
     gf = GetFollowers()
-    print(gf.followers)
-    followers = gf.get_following('3519515154')
+    followers = gf.get_following('3519515154', max_pages=2)
     print(followers)
-    gf.follow_users(followers[:10])
+
+    gf.follow_users(followers[:2])
