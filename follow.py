87412c99187ac6447c8d4b7ebb7c41782611955d
Matthew Eng <matthew.eng2@gmail.com>
2019-08-21 22:52:54 -0400

    Fix error in if __name__=='__main__'

--- follow.py
+++ follow.py
@@ -12,12 +12,13 @@ class GetFollowers():
         self.instaAPI.login()
         self.followers = []
 
-    def get_following(self, username_id, max_pages=10, max_id=''):
+    def get_following(self, username_id, max_pages=1, max_id=''):
         ''' gets the Followers for a given account '''
 
         next_max_id = ''
         i = 0
 
+        print('test')
         while i <= max_pages:
             i+= 1
             self.instaAPI.getUserFollowers(username_id, maxid=next_max_id)
@@ -39,6 +40,14 @@ class GetFollowers():
         for user_id in user_ids:
             self.instaAPI.follow(user_id)
 
+    def unfollow_users(self, num_user_to_unfollow=10):
+        ''' Unfollow random sample of users from following '''
+
+        self.following = self.instaAPI.getSelfFollowing()
+
+        for user_id in self.following:
+            self.instaAPI.unfollow(user_id)
+
 def get_list_of_users():
     ''' Returns list of user ids from database '''
 
@@ -52,8 +61,12 @@ def get_list_of_users():
 
     return r
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
+
+    # print(get_list_of_users())
 
     gf = GetFollowers()
-    followers = gf.get_following()
-    gf.follow_user(followers)
+    print(gf.followers)
+    followers = gf.get_following('3519515154')
+    print(followers)
+    gf.follow_users(followers[:10])
